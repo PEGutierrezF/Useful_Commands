0d198813d693639c150a917ae3d725dfe8a7bf9e
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-04-13 18:17:13 -0400

    data

---
+++ Themes.R
@@ -0,0 +1,26 @@
+
+
+
+
+# ---------------------------------------------
+# Change Themes
+# 13 Apr 2022
+# Pablo E. Gutiérrez-Fonseca
+# pabloe.gutierrezfonseca@gmail.com
+# ---------------------------------------------
+#  
+
+# https://www.garrickadenbuie.com/project/rsthemes/
+
+Installation#
+# You can install rsthemes from GitHub with:
+
+# install.packages("devtools")
+devtools::install_github("gadenbuie/rsthemes")
+rsthemes::install_rsthemes()
+
+rsthemes::list_rsthemes()
+rsthemes::try_rsthemes("light")
+rstudioapi::applyTheme("base16 Macintosh {rsthemes}")
+
+
